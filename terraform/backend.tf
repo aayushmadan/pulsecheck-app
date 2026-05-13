cf90e9c6e67caaf7210ac8e1ddf2522666cbd3b4
aayushmadan <aayush.madan4611@gmail.com>
2026-05-13 17:51:41 +0530

    Fix: Bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-bucket-867345910980-ap-southeast-2-an "
+    bucket = "tf-state-bucket-867345910980-ap-southeast-2-an"
     key    = "./terraform/terraform.tfstate"
     region = "ap-southeast-2"
   }
